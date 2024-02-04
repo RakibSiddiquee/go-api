8709ec5b42229006ae739a234409a37a8b70e965
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-02-05 01:49:26 +0600

    Token table and model added

--- internal/data/models.go
+++ internal/data/models.go
@@ -5,9 +5,21 @@ import "time"
 type User struct {
 	ID        int       `json:"id"`
 	Email     string    `json:"email"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
+	FirstName string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdateAt  time.Time `json:"updated_at"`
+	Token     Token     `json:"token"`
+}
+
+type Token struct {
+	ID        int       `json:"id"`
+	UserId    int       `json:"user_id"`
+	Email     string    `json:"email"`
+	Token     string    `json:"token"`
+	TokenHash []byte    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdateAt  time.Time `json:"updated_at"`
+	Expiry    time.Time `json:"expiry"`
 }
