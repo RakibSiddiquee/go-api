74e715bc83056ca244428ec1828da0a2dba0a74b
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-02-24 20:39:46 +0600

    test token and save token added in routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"go-api/internal/data"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -54,6 +55,59 @@ func (app *application) routes() http.Handler {
 		app.infoLog.Println("Got back id of", id)
 		newUser, _ := app.models.User.GetOne(id)
 		app.writeJSON(w, http.StatusOK, newUser)
+	})
+
+	mux.Get("/test-generate-token", func(w http.ResponseWriter, r *http.Request) {
+		token, err := app.models.User.Token.GenerateToken(1, 60*time.Minute)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		token.Email = "admin@admin.com"
+		token.CreatedAt = time.Now()
+		token.UpdateAt = time.Now()
+
+		payload := jsonResponse{
+			Error:   false,
+			Message: "success",
+			Data:    token,
+		}
+
+		app.writeJSON(w, http.StatusOK, payload)
+	})
+
+	mux.Get("/test-save-token", func(w http.ResponseWriter, r *http.Request) {
+		token, err := app.models.User.Token.GenerateToken(1, 60*time.Minute)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		user, err := app.models.User.GetOne(1)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		token.UserId = user.ID
+		token.CreatedAt = time.Now()
+		token.UpdateAt = time.Now()
+
+		err = token.Insert(*token, *user)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		payload := jsonResponse{
+			Error:   false,
+			Message: "success",
+			Data:    token,
+		}
+
+		app.writeJSON(w, http.StatusOK, payload)
+
 	})
 
 	return mux
