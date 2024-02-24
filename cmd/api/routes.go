c09e254900e8c25e7f5e45591ccd538010b720ff
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-02-24 20:13:48 +0600

    Add user route and method added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -34,5 +34,27 @@ func (app *application) routes() http.Handler {
 		app.writeJSON(w, http.StatusOK, all)
 	})
 
+	mux.Get("/users/add", func(w http.ResponseWriter, r *http.Request) {
+		var u = data.User{
+			Email:     "you@there.com",
+			FirstName: "You",
+			LastName:  "There",
+			Password:  "password",
+		}
+
+		app.infoLog.Println("Adding user...")
+
+		id, err := app.models.User.Insert(u)
+		if err != nil {
+			app.errorLog.Panicln(err)
+			app.errorJSON(w, err, http.StatusForbidden)
+			return
+		}
+
+		app.infoLog.Println("Got back id of", id)
+		newUser, _ := app.models.User.GetOne(id)
+		app.writeJSON(w, http.StatusOK, newUser)
+	})
+
 	return mux
 }
