e1ecff529d6caabfd972d9535807485de338a40a
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-02-28 01:24:15 +0600

    AllUsers handler added

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"go-api/internal/data"
 	"net/http"
 	"time"
 )
@@ -98,4 +99,21 @@ func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
+	var users data.User
+	all, err := users.GetAll()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "success",
+		Data:    envelope{"users": all},
+	}
+
+	app.writeJSON(w, http.StatusOK, payload)
 }
